586b9519185dd44f8367e2c70dfab8f6397ae13a
Jonathan Manning <jmanning@ebi.ac.uk>
2020-06-15 16:32:52 +0100

    Fix script name

--- bin/submitTertiaryWorkflow.sh
+++ bin/submitTertiaryWorkflow.sh
@@ -39,7 +39,9 @@ else
     export FLAVOUR=w_smart-seq_clustering
 fi
 
-run_flavour_workflows.sh
+# This script is under /bin of the scxa-workflows repo
+run_tertiary_workflow.sh
+
 if [ $? -eq 0 ]; then
     mkdir -p matrices
                     
