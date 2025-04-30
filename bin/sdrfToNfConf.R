dc636c4e0db4c32b39c3c94bb7714fd88a637d52
Anil Thanki <thanki.anil@gmail.com>
2025-04-30 10:10:55 +0100

    Update sdrfToNfConf.R - add logs

--- bin/sdrfToNfConf.R
+++ bin/sdrfToNfConf.R
@@ -231,8 +231,10 @@ if (! is.null(opt$cell_meta_fields)){
         for (ctf in cell_type_fields){
             actf <- getActualColnames(ctf, cell.meta.source)
             if (! is.null(actf)){
+                pinfo(actf," cell.meta.source")
                 cell.type.col <- c(cell.type.col, actf)
             }
+            pinfo(cell.type.col)
         }
     }
     
